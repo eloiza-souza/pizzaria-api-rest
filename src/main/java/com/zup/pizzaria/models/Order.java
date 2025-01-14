814d533f4757bc3c53e7908135ae9f09db188c5c
Eloiza <eloiza.souza@zup.com.br>
2025-01-14 16:14:30 -0300

    feat: create Order class

---
+++ src/main/java/com/zup/pizzaria/models/Order.java
@@ -0,0 +1,44 @@
+package com.zup.pizzaria.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String description;
+    private Long clientId;
+
+    public Order(String description, Long clientId) {
+        this.description = description;
+        this.clientId = clientId;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+}
